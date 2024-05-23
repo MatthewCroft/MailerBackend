88ba9e9589ff56fa1217bd6134162e05832a4390
Matthew Croft <matthewpcroft1@gmail.com>
2024-05-22 21:52:13 -0400

    summary BART model deployed on aws, and successfully called through spring boot endpoint, see example in model.http

---
+++ src/main/java/com/croft/mailerbackend/MailerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.croft.mailerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/croft/mailerbackend/MailerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.croft.mailerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
